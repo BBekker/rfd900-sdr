c06ebe50003867c305656c14dd685e9dbf3da71f
Bernard Bekker <bernard@bernardbekker.nl>
2018-03-29 18:10:01 +0200

    update

--- test.m
+++ test.m
@@ -1,65 +1,27 @@
 close all; clear;
 
-[x, Fs] = readfile('IQ_1pkt.wav');
-t = linspace(0,length(x)/Fs, length(x))';
-length(t)
-
-%offset = -250000;
-offset = meanfreq(x,Fs);
-channelspacing = 200000;
-
-%%Tune signal
-
-s = exp(-1i * pi * 2 * offset * t);
-x = x .* s;
-
-%spectrogram(x,1024)
-%%LPF and resample
-
-[b,a] = butter(4,2*channelspacing / Fs);
-x_f = filter(b,a,x);
-x_r = resample(x_f, 4*channelspacing, Fs);
-
-
-Fs2 = Fs /( Fs / (4*channelspacing))
-
-t2 = linspace(0,length(x_r)/Fs2, length(x_r))';
-%% get bits
-
-res = signaltobits(x_r,Fs2, 0.001);
-
-%% Find Preamble
-preamblemask = [0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1];
-preamblepos = strfind(res, preamblemask)
-
-preamblestart = preamblepos(1);
-preambleend = preamblepos(end) + length(preamblemask) - 1
-
-%plot(linspace(1,length(preamblematch)), preamblematch');
-
-%% Find sync word
-%sync code from si1000 datasheet & sik sourcecode
-syncword = [0 0 1 0 1 1 0 1 1 1 0 1 0 1 0 0];
-syncpos = strfind(res, syncword)
-headerstart = syncpos + length(syncword)
-
-%% dewhiten/decode data
-% lfsr reverse engineered from output with known cleartext
-lfsr = comm.PNSequence('Polynomial',[9 5 0], 'InitialConditions', [0 1 1 1 1 0 0 0 0], 'SamplesPerFrame',8);
-cursor = headerstart;
-header3 = bi2de(xor(res(cursor:cursor+7), lfsr.step()'),'left-msb')
-cursor = cursor + 8;
-header2 = bi2de(xor(res(cursor:cursor+7), lfsr.step()'),'left-msb')
-cursor = cursor + 8;
-packetlen = bi2de(xor(res(cursor:cursor+7), lfsr.step()'),'left-msb')
-cursor  = cursor +8;
-
-if( cursor + packetlen*8 > length(res))
-    print('packet size error');
+[x, Fs] = readfile('IQ_2short.wav');
+haspacket = filtfilt(ones(1,4)./4,1,abs(x)) > 0.2;
+length(x)
+packets = [];
+cnt = 0;
+for i=1:length(haspacket)
+    if(haspacket(i) == 0)
+        if(cnt > 10)
+            packets(end,2) = i;
+        end
+        cnt = 0;
+    else
+        cnt = cnt + 1;
+    end
+    if(cnt == 10) 
+        packets = [packets; i 0];
+    end
 end
 
-data = reshape(res(cursor:cursor + packetlen*8 - 1),8, [])'
-bytes = zeros(0,size(data,1))
-for i = 1 : size(data,1)
-    bytes(i) = bi2de( xor(data(i,:),lfsr.step()'),'left-msb');
+
+for i = 1:size(packets,1)
+    if packets(i,2) - packets(i,1) > 1000
+        bytes = decodepacket(x(packets(i,1):packets(i,2)),Fs)
+    end
 end
